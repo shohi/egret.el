2c535b9c3c9be52634bdab84c8e9e8f46e76ae86
shohi <info@oshohi.org>
2026-09-19 12:40:58 -0400

    update readme and add LICENSE

--- egret.el
+++ egret.el
@@ -4,7 +4,7 @@
 
 ;; Author: Shohi
 ;; URL: https://github.com/shohi/egret.el
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "31.1"))
 ;; Keywords: languages, go, tests, tools
 
